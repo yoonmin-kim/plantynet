656be41b2acd62c99a2cdedeadb080f393997ea2
yoonmin-kim <rladbsals23@gmail.com>
2021-11-18 22:43:00 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## 목표 : (주)플랜티넷 자격요건에 해당하는 기술들을 활용하여 간단한 CRUD구현
+
+### 기능목록
+* 상품등록
+* 상품목록
+* 상품수정
+* 상품삭제
+
+### Skills & Tools
+* Spring Boot
+* JSP/JSTL
+* Thymeleaf
+* React
+* MySQL, Mybatis
+* gradle
+* IntelliJ
+
+
+
+
+
+
---
+++ src/main/java/hello/com/plantynet/PlantynetApplication.java
@@ -0,0 +1,13 @@
+package hello.com.plantynet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantynetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantynetApplication.class, args);
+	}
+
+}
